3e505f7abc910068227012557855b71d6f0845c7
ivo <ivailo1996g@abv.bg>
2020-04-07 19:30:29 -0400

    game Logic

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,11 +1,16 @@
 
 
 
-console.log("running");
 
-var cardOne="queen";
-var cardTwo="king";
-let cardThree="queen";
-let cardFour="king";
+let cards= ["queen", "queen", "king", "king"];
+let cardsInPlay= [];
+let CardOne= cards[0];
+let CardTwo= cards[2];
+cardsInPlay.push(CardOne);
+cardsInPlay.push(CardTwo);
 
-console.log("User fliped "+cardOne);
+console.log("User fliped "+cards[0]);
+console.log(cardsInPlay);
+
+if(cardsInPlay.length == 2)
+{ CardOne==CardTwo? console.log("Found a match"): console.log("not Yet");}
