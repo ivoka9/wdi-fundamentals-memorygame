3d84988093618056137a233fcd831ba1acda669c
ivo <ivailo1996g@abv.bg>
2020-04-08 15:47:47 -0400

    MORE game logic

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,6 +1,19 @@
 
 
-let cards= ["queen", "queen", "king", "king"];
+let cards= [
+  {rank: "king",
+  suit: "hearts",
+cardImage:"images/king-of-diamonds.png"},
+  {rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-hearts.png"},
+  {rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"},
+  {rank: "queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png"}
+];
 let cardsInPlay= [];
 
 function  checkForMatch(){
@@ -14,8 +27,8 @@ function  checkForMatch(){
 
 function flipCard (cardId){
 
-console.log("User fliped " +cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User fliped " +cards[cardId].rank+"  "+cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 if(cardsInPlay.length==2){checkForMatch();}
 
                           }
