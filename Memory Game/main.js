72a050067aad7195b823ae0383cc07404a616520
ivo <ivailo1996g@abv.bg>
2020-04-10 02:56:17 -0400

    a lot of bugs

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,4 +1,29 @@
+let cardElement;
+let cardsInPlay= [];
+var cardId;
 
+function works(){
+  console.log("works")
+}
+// TRIED but it didint work.. the event lisener never lisend...
+/*
+function createBoard() {
+for(let i=0; i<4 ; i++){
+
+   cardElement = document.createElement('img');
+ cardElement.setAttribute('src','images/back.png');
+ cardElement.setAttribute('data-id',i);
+  cardElement.addEventListener('click ',flipCard ());
+
+ document.getElementById('game-board').appendChild(cardElement);
+
+ console.log(cardElement);
+
+}
+
+// cardElement.setAttribute('src','images/back.png');
+}
+*/
 
 let cards= [
   {rank: "king",
@@ -14,31 +39,53 @@ cardImage:"images/queen-of-hearts.png"},
 suit: "diamonds",
 cardImage:"images/queen-of-diamonds.png"}
 ];
-let cardsInPlay= [];
+
 
 function  checkForMatch(){
+
+
   if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
+
   } else {
-  console.log("Sorry, try again.");
+console.log("Sorry, try again.");
+
    }
                         }
 
 
-function flipCard (cardId){
+function flipCard (n){
 
-console.log("User fliped " +cards[cardId].rank+"  "+cards[cardId].cardImage);
+switch (n){
+
+  case 'QoD' :
+ console.log("works");
+ cardId=3 ;
+ document.getElementById('QoD').src= "images/queen-of-diamonds.png";
+break;
+
+    case 'QoH' :
+    document.getElementById('QoH').src= "images/queen-of-hearts.png";
+cardId=2 ;
+break;
+
+      case 'KoD' :
+      document.getElementById('KoD').src= "images/king-of-diamonds.png";
+cardId=1 ;
+break;
+      case 'KoH' :
+      document.getElementById('KoH').src= "images/king-of-hearts.png";
+cardId=0 ;
+break;
+}
 cardsInPlay.push(cards[cardId].rank);
+
 if(cardsInPlay.length==2){checkForMatch();}
 
                           }
 
-flipCard(1);
-flipCard(3);
-
 
 console.log(cardsInPlay);
 
-/*  if(cardsInPlay.length == 2)
-{ CardOne==CardTwo? console.log("Found a match"): console.log("not Yet");}
-*/
+
+ // createBoard();
